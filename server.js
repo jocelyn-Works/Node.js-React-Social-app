75981803f0cd919fd291bf3250fcff3d18c67291
jocelyn <grelierjocelyn@gmail.com>
2024-01-16 13:59:46 +0100

    routes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes.js');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// routes
+app.use('/api/user' , userRoutes);
 
 
+
+
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`application Node : http://127.0.0.1:${process.env.PORT}`);
 })
