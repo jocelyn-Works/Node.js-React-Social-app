abc4511d3cb5478819654269ac2565bb83b8aa6a
jocelyn <grelierjocelyn@gmail.com>
2024-01-17 15:19:20 +0100

    Post-router

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,2 +1,9 @@
 const router = require('express').Router();
 const postController = require("../controllers/post.controller");
+
+router.post("/", postController. createPost);
+router.get("/", postController. readPost);
+router.put("/:id", postController. updatePost);
+router.delete("/:id", postController. deletePost);
+
+module.exports = router;
