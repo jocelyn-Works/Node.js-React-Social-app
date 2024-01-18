428f61f4828d45588095fe8525e46cb76f8c107c
jocelyn <grelierjocelyn@gmail.com>
2024-01-18 09:44:57 +0100

    Post-controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -20,30 +20,56 @@ module.exports.createPost = async (req, res) => {
   }
 };
 
-module.exports.readPost = (req, res) => {
-  PostModel.find((err, docs) => {
-    if (!err) res.send(docs);
-    else console.log("erreur pour obtenir des données : " + err);
-  });
-};
-
-module.exports.updatePost = (req, res) => {
-  if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send("ID Inconnue : " + req.params.id);
-
-  const updatedRecord = {
-    message: req.body.message,
+module.exports.readPost = async (req, res) => {
+    try {
+      const docs = await PostModel.find();
+      res.send(docs);
+    } catch (err) {
+      console.error("Erreur lors de la récupération des données : " + err);
+      res.status(500).json({ message: "Erreur lors de la récupération des données" });
+    }
   };
 
-  PostModel.findByIdAndUpdate(
-    req.params.id,
-    { $set: updatedRecord },
-    { new: true },
-    (err, docs) => {
-      if (!err) res.send(docs);
-      else console.log("Erreure de mise a jour : " + err);
+module.exports.updatePost = async (req, res) => {
+    try {
+      if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID Inconnue : " + req.params.id);
+  
+      const updatedRecord = {
+        message: req.body.message,
+      };
+  
+      const doc = await PostModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updatedRecord },
+        { new: true }
+      );
+  
+      if (doc) {
+        res.send(doc);
+      } else {
+        res.status(404).send("Post non trouvé");
+      }
+    } catch (err) {
+      console.error("Erreur de mise à jour : " + err);
+      res.status(500).json({ message: "Erreur lors de la mise à jour du post" });
     }
-  );
-};
+  };
 
-module.exports.deletePost = (req, res) => {};
+  module.exports.deletePost = async (req, res) => {
+    try {
+      if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID Inconnue : " + req.params.id);
+  
+      const deletedPost = await PostModel.findByIdAndDelete(req.params.id);
+  
+      if (deletedPost) {
+        res.send(deletedPost);
+      } else {
+        res.status(404).send("Post non trouvé");
+      }
+    } catch (err) {
+      console.error("Erreur de suppression : " + err);
+      res.status(500).json({ message: "Erreur lors de la suppression du post" });
+    }
+  };
